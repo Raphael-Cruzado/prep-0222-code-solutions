177f7f02c20a09dd527706ecda5898fca45ba0ff
Lance Cruzado <raphaellcruzado@gmail.com>
2022-02-28 05:42:14 +0000

    prep-javascript-arrays -Lance Cruzado

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,15 @@
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+
+console.log(languages);
+
+var firstElement = languages[0];
+console.log('the first element of the array is ' + firstElement);
+
+var thirdElement = languages[2];
+console.log('the 3rd element of the array is ' + thirdElement);
+
+var length = languages.length;
+console.log('The length of the array is ' + length);
+
+var lastElement = languages[length - 1];
+console.log('The last element of the array is ' + lastElement);
